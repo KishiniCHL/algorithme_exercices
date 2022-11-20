4c2bf54340757a7744c4bc7ccde31a740f8f0897
Hoai-lan DINH VU <christiane.dinhvu@gmail.com>
2022-11-20 23:18:01 +0100

    fixes

--- exo_nim.js
+++ exo_nim.js
@@ -8,14 +8,14 @@ class player {
 let joueur1 = new player('Joueur 1', true)
 let joueur2 = new player('Joueur 2', false)
 
-var nb=0;
+let nb=0;
 var jeu=true
-var container = document.getElementById('container')
-var allumette_container = document.getElementById('alumette_container')
-var turn = document.getElementById('who_plays')
+var container = document.querySelector('#container')
+var allumette_container = document.querySelector('#alumette_container')
+var turn = document.querySelector('#who_plays')
 
 function return_value(){
-    var nb = document.getElementById("nb").value;    
+    nb = document.querySelector("#nb").value;    
 
     if(nb<=3 || isNaN(nb)){
         alert("Veuillez entrer un chiffre supérieur à 3");
@@ -31,7 +31,7 @@ function return_value(){
 }
 
 function duplicate_allumette() {
-    const node = document.getElementById("allumette");
+    const node = document.querySelector("#allumette");
     const clone = node.cloneNode(true);
     allumette_container.appendChild(clone);
 }
@@ -41,8 +41,7 @@ function create_button(){
 }
 
 function remove_allumette(){
-    var nb = document.getElementById("nb").value;
-    var get_removed= document.getElementById("nbr_removed");
+    var get_removed= document.querySelector("#nbr_removed");
     var nbr_removed = parseInt(get_removed.value)
 
     if(nbr_removed>3 || nbr_removed<=0 || isNaN(nbr_removed)){
@@ -56,15 +55,14 @@ function remove_allumette(){
         }else{
             turn.innerHTML="Joueur 2 joue"
         }
-        
-        
-        for (var x = nbr_removed; x >= 0; x--) {
-            allumette_container.removeChild(allumette_container.childNodes[x]);
+        for (var x = nbr_removed; x >= 1; x--) {
+            allumette_container.removeChild(allumette_container.childNodes[1]);
         }
         nb=nb-nbr_removed;
+        console.log(nb)
     }
 
-    var nbr_removed = document.getElementById("nbr_removed").value="";
+    var nbr_removed = document.querySelector("#nbr_removed").value="";
     
     if(nb===0){ 
         turn.innerHTML=""
